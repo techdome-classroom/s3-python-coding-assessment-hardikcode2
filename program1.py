145e1680f271f957f7e0afaef333c133b419a99a
hardikcode2 <moneybro02@gmail.com>
2024-09-30 15:27:01 +0530

    "Implemented solutions for two programming problems: a parentheses validation function and a Roman numeral to integer converter."

--- program1.py
+++ program1.py
@@ -4,7 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        stack = []
+        paren_map = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in paren_map:
+                top_element = stack.pop() if stack else '#'
+                if paren_map[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
 
 
 
